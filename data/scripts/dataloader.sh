fe9a80a647d52700fffcdd137dceec65b3904880
chldyddnjs <dyddnjsdl321@naver.com>
2025-08-01 14:18:10 +0900

    Fixed scripts error

--- data/scripts/dataloader.sh
+++ data/scripts/dataloader.sh
@@ -1,4 +1,1 @@
-python -m data.dataset_hf --speed medium --vocalinstrumental vocal --gender female --lang en --include lyrics
-python -m data.dataset_hf --speed medium --vocalinstrumental vocal --gender male --lang en --include lyrics
-python -m data.dataset_hf --speed high --vocalinstrumental vocal --gender male --lang en --include lyrics
-python -m data.dataset_hf --speed high --vocalinstrumental vocal --gender female --lang en --include lyrics
+python -m data.dataset_hf
